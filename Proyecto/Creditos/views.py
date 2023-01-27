17e24ea30bf296246920f4a82f0dd1a3402af25e
Facundo <fa,rodriguez.castro@gmail.com>
2023-01-27 10:45:53 -0300

    mostrar_deudor despliega nombre, apellido y deuda

--- Proyecto/Creditos/views.py
+++ Proyecto/Creditos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from Creditos.admin import DeudoresAdmin
 from .models import Deudores
 
+
 def mostrar_deudores(request):
     if request.method == 'GET':
         deudores = Deudores.objects.all()
